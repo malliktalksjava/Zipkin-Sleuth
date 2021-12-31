b546450a31743933447eb88312c04308932fd6c2
Venkata183 <vnedunooru1990@gmail.com>
2021-12-31 15:17:39 +0530

    Added services 1,2,3

---
+++ service-3/src/main/java/com/mvtechbytes/service3/Service3Application.java
@@ -0,0 +1,44 @@
+package com.mvtechbytes.service3;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Service3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service3Application.class, args);
+	}
+}
+
+@RestController
+class Service1Controller{
+	
+	@Autowired
+	RestTemplate restTemplate;
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}@Bean
+	public AlwaysSampler alwaysSampler() {
+		return new AlwaysSampler();
+	}
+	private static final Logger LOG = Logger.getLogger(ZipkinController.class.getName());
+	
+	@GetMapping(value="/service3")
+	public String service1() {
+		LOG.info("Inside Service 3..");
+		
+		
+		return "Request reached third service";
+	}
+}
